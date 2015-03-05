e163eed5388f608584d6a19d0ae0c9eb51924bca
Zhongpeng Lin (林中鹏) <lin.zhp@gmail.com>
2015-03-04 22:03:15 -0800

    Calculate average clustering coefficient for very degree

--- codevo/graph_analysis.py
+++ codevo/graph_analysis.py
@@ -1,7 +1,9 @@
 __author__ = 'zplin'
 import sys
 import json
-from networkx import Graph, transitivity
+import csv
+import numpy as np
+from networkx import Graph, transitivity, clustering
 from networkx.readwrite import json_graph
 
 
@@ -12,3 +14,16 @@ if __name__ == '__main__':
     print('Number of nodes:', g.number_of_nodes())
     print('Average degrees:', 2 * g.number_of_edges()/g.number_of_nodes())
     print('Transitivity:', transitivity(g))
+    # Calculating average clustering coefficient for different degrees
+    cc = clustering(g)
+    degree_cc = {}
+    for node, degree in g.degree_iter():
+        if degree not in degree_cc:
+            degree_cc[degree] = []
+        degree_cc[degree].append(cc[node])
+
+    with open('output/clustering.csv', 'w', newline='') as cc_file:
+        writer = csv.DictWriter(cc_file, ['degree', 'average_cc'])
+        writer.writeheader()
+        for degree in degree_cc:
+            writer.writerow({'degree': degree, 'average_cc': np.mean(degree_cc[degree])})
